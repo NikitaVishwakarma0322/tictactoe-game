c06cebf3b877dd0663b50b5f74cdf7cf216f17ba
Nikita <Nikitavishwakarma304@gmail.com>
2023-07-30 23:47:04 +0530

    display 'X' when clicking on the Square

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import './styles.scss';
-import { useState } from 'react';
-import Board from './components/board';
+// import { useState } from 'react';
+import Board from './components/Board';
 
 function App() {
-  const [counter, setCounter] = useState(1);
-  console.log('Hello');
-  const onBtnClick = () => {
-    setCounter(currentCounter => {
-      return currentCounter + 1;
-    });
-  };
   return (
-    <div>
-      <button onClick={onBtnClick}>Click me please</button>
-      <div>{counter}</div>
+    <div className="app">
+      <Board />
     </div>
   );
 }
